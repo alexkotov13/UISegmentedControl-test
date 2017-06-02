aae2e2727c93e94765f934f5c6d29d14e2d63341
Alexey <alexey@mac-pro-user.local>
2017-06-02 05:48:18 -0700

    Initial Commit

---
+++ UISegmentedControl test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UISegmentedControl test
+//
+//  Created by Alexey on 02.06.17.
+//  Copyright © 2017 Alexey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UISegmentedControl testTests/UISegmentedControl_testTests.m
@@ -0,0 +1,39 @@
+//
+//  UISegmentedControl_testTests.m
+//  UISegmentedControl testTests
+//
+//  Created by Alexey on 02.06.17.
+//  Copyright © 2017 Alexey. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UISegmentedControl_testTests : XCTestCase
+
+@end
+
+@implementation UISegmentedControl_testTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
